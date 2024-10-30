cc3ea5e007decaf0bee023a7b3453f28ace7a0eb
Nishul Dhakar <nishuldhakar.se@gmail.com>
2024-10-30 20:55:11 +0530

    added gsap

--- script.js
+++ script.js
@@ -7,3 +7,32 @@ hamburger.addEventListener('click', () => {
     bars.classList.toggle('fa-xmark');
 });
 
+var tl = gsap.timeline()
+
+tl.from("#logo", {
+    y: -20,
+    opacity: 0,
+    duration: 0.5,
+    delay: 0.5,
+});
+
+tl.from("ul ", {  
+    y: -20,
+    opacity: 0,
+    duration: 0.5,
+    stagger: 0.3,
+});
+
+tl.from("h1 ", {  
+    y: -10,
+    opacity: 0,
+    duration: 0.5,
+});
+
+
+tl.from("img ", {  
+    y: 20,
+    opacity: 0,
+    duration: 1,
+});
+
